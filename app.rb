3c1db3d302648eaf4d9425bdb145822dae89b666
David Anderson <matandersonlv@hotmail.com>
2019-06-25 20:07:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,12 @@ class App < Sinatra::Base
   end
   
   post '/team' do
- 
+    @name = params[:name]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
     erb :team
   end
 
